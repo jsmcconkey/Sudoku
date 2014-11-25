c5486e64d9f5f937e390bd13baf5392cf439348d
James McConkey <James@Jamess-MacBook-Pro.local>
2014-11-24 21:09:56 -0700

    not working score page

--- src/Menu/UserScores.java
+++ src/Menu/UserScores.java
@@ -7,6 +7,7 @@ import java.awt.*;
 public class UserScores extends JPanel{
 	public JLabel message;
 	public JButton backButton;
+	public JTable userScores;
 	
 	public UserScores()
 	{
@@ -25,6 +26,30 @@ public class UserScores extends JPanel{
 		backButton.setBackground(new Color(102,255,255));
 		backButton.setBounds(this.getWidth()/20,this.getHeight()/20,150,50);
 		add(backButton);
+		
+		String[] columnNames = {"User", "Score"};
+		String[][] topScores = getTopScores();
+		userScores = new JTable(topScores, columnNames);
+		userScores.setPreferredScrollableViewportSize(new Dimension(200,200));
+		userScores.setBounds(this.getWidth()/2-200, 50, 400, 200);
+		userScores.setRowHeight(20);
+		JScrollPane scrollPane = new JScrollPane(userScores);
+		userScores.setBounds(this.getWidth()/2-200, 50, 400, 200);
+		userScores.setFillsViewportHeight(true);
+		userScores.setVisible(true);
+		scrollPane.setVisible(true);
+
+		add(scrollPane);
+		
+	}
+	
+	public String[][] getTopScores()
+	{
+		String [] firstScore = {"Steven McNutts", "20"};
+		String [] secondScore = {"Slippery McBalls", "15"};
+		String[][] returnData = {firstScore, secondScore};
+		
+		return returnData;
 	}
 	
 }
